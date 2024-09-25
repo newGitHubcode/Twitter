bf4c6a960300a73d60f0f683a365a4cd62c59a45
coder <qdasdsadsada@gmail.com>
2024-09-25 07:37:10 +0700

    centered logo
    yes

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -23,7 +23,7 @@ export default async function Login() {
 
     // authbutton
     return (
-        <div className="flex-1 flex justtify-center items-center">
+        <div className="flex-1 flex justify-center items-center">
             <GitHubButton /> 
         </div>
         
